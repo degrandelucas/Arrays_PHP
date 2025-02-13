db35d1c4fb9e3ba1ba4c8a75978c6d0b2440a873
degrandelucas <lucasdegrande15@gmail.com>
2025-02-13 18:40:53 -0300

    desenvolvimento da logica para PercorrerArrayNotas

--- PercorrerArrayNotas.php
+++ PercorrerArrayNotas.php
@@ -1,1 +1,11 @@
 <?php
+
+$notas = [9, 3, 10, 5, 10, 8.5, 9, 10, 8, 7, 6.5, 5, 4.8];
+
+$quantidadeNotas = count($notas);
+
+echo "Quantidade de notas no array: $quantidadeNotas" . PHP_EOL;
+
+foreach ($notas as $nota){
+    echo "Nota: $nota" . PHP_EOL;
+}
