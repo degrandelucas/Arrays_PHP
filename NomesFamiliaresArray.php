a023becd60ed80fdb1ee984422b2447b8e493a70
degrandelucas <lucasdegrande15@gmail.com>
2025-02-14 09:19:45 -0300

    desenvolvimento da logica para adicionar de um novo item ao array

--- NomesFamiliaresArray.php
+++ NomesFamiliaresArray.php
@@ -4,6 +4,8 @@ $nomesFamiliares = ["Lucas", "Dany", "Mateus", "Marcos"];
 
 echo "Nomes familiares: " . implode(", ", $nomesFamiliares) . "\n" . PHP_EOL;
 
+$nomesFamiliares[] = "Lilian";
+
 echo "Nomes familiares: " . PHP_EOL;
 foreach ($nomesFamiliares as $nome){
     echo "Nome: $nome" . PHP_EOL;
